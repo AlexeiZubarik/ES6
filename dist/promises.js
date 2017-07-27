2d221ee36473e56d76c0463f7fb88ad0eed04bed
Alexei Zubarik <alesha.zubarik@yandex.ru>
2017-07-27 08:50:52 +0300

    promises

---
+++ dist/promises.js
@@ -0,0 +1,35 @@
+'use strict';
+
+function applyForVisa(documents) {
+  console.log('Обработка заявления...');
+  var promise = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      Math.random() > 0 ? resolve({}) : reject('В визе отказано: не хватает документов');
+    }, 2000);
+  });
+  return promise;
+}
+
+function getVisa(visa) {
+  console.info('Виза получина');
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      return resolve(visa);
+    }, 2000);
+  });
+}
+
+function bookHotel(visa) {
+  console.log(visa);
+  console.log('Бронируем отель');
+  return Promise.resolve(visa);
+}
+
+function buyTickets(booking) {
+  console.log('Покупаем билеты');
+  console.log('Бронь', booking);
+}
+
+applyForVisa({}).then(getVisa).then(bookHotel).then(buyTickets).catch(function (error) {
+  return console.error(error);
+});
